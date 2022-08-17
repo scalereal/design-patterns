673879732b87e83ba6d4961ebdcff50d02625d9c
Sandesh <sandeshbodake121@gmail.com>
2022-08-17 12:30:01 +0530

    SB | Facade pattern fix interfaces

--- kotlin/Structural/Facade.kt
+++ kotlin/Structural/Facade.kt
@@ -1,49 +1,68 @@
-// These are some of the classes of a complex 3rd-party video
-// conversion framework. We don't control that code, therefore
-// can't simplify it.
+import BitrateReader.convert
+import BitrateReader.read
+import CodecFactory.extract
+import java.io.File;
 
-class VideoFile {
-
+class VideoFile(private val name: String) {
+    val codecType: String = name.substring(name.indexOf(".") + 1)
 }
 
-class OggCompressionCodec {
-
+interface Codec {
 }
 
-class MPEG4CompressionCodec {
-
+class MPEG4CompressionCodec : Codec {
+    var type = "mp4"
 }
 
-class CodecFactory {
-
+class OggCompressionCodec : Codec {
+    var type = "ogg"
 }
 
-class BitrateReader {
+object CodecFactory {
+    fun extract(file: VideoFile): Codec {
+        val type = file.codecType
+        return if (type == "mp4") {
+            println("CodecFactory: extracting mpeg audio...")
+            MPEG4CompressionCodec()
+        } else {
+            println("CodecFactory: extracting ogg audio...")
+            OggCompressionCodec()
+        }
+    }
+}
 
+object BitrateReader {
+    fun read(file: VideoFile, codec: Codec?): VideoFile {
+        println("BitrateReader: reading file...")
+        return file
+    }
+
+    fun convert(buffer: VideoFile, codec: Codec?): VideoFile {
+        println("BitrateReader: writing file...")
+        return buffer
+    }
 }
 
 class AudioMixer {
-
+    fun fix(result: VideoFile?): File {
+        println("AudioMixer: fixing audio...")
+        return File("tmp")
+    }
 }
 
-// We create a facade class to hide the framework's complexity
-// behind a simple interface. It's a trade-off between
-// functionality and simplicity.
-
 class VideoConversionFacade {
-    fun convertVideo(fileName: String, format: String): File {
+    fun convertVideo(fileName: String?, format: String): File {
         println("VideoConversionFacade: conversion started.")
-        val file = VideoFile(fileName)
-        val sourceCodec: Codec = CodecFactory.extract(file)
-        val destinationCodec: Codec
-        destinationCodec = if (format == "mp4") {
+        val file = VideoFile(fileName!!)
+        val sourceCodec = extract(file)
+        val destinationCodec: Codec = if (format == "mp4") {
             MPEG4CompressionCodec()
         } else {
             OggCompressionCodec()
         }
-        val buffer: VideoFile = BitrateReader.read(file, sourceCodec)
-        val intermediateResult: VideoFile = BitrateReader.convert(buffer, destinationCodec)
-        val result: File = AudioMixer().fix(intermediateResult)
+        val buffer = read(file, sourceCodec)
+        val intermediateResult = convert(buffer, destinationCodec)
+        val result = AudioMixer().fix(intermediateResult)
         println("VideoConversionFacade: conversion completed.")
         return result
     }
@@ -51,6 +70,6 @@ class VideoConversionFacade {
 
 fun main(args: Array<String>) {
     val converter = VideoConversionFacade()
-    val mp4Video: File = converter.convertVideo("youtubevideo.ogg", "mp4")
+    val mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4")
     // ...
 }
